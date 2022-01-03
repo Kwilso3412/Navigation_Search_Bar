8927351e6fd10c60aef828575a3da1fca9d48c37
Kwilso3412 <80131328+Kwilso3412@users.noreply.github.com>
2022-01-03 17:18:58 -0500

    updates for app finally complete

--- app/src/main/java/com/example/navigationsearchbar/MainActivity.kt
+++ app/src/main/java/com/example/navigationsearchbar/MainActivity.kt
@@ -2,34 +2,45 @@ package com.example.navigationsearchbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.widget.*
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    //adds teh list when the app is opened
     private lateinit var adapter: ArrayAdapter<*>
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
 
-
-        // to fix the list view try these suggestions https://www.raywenderlich.com/155-android-listview-tutorial-with-kotlin
-
-        adapter = ArrayAdapter( this, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.countries_array))
-        ListView.adapter = adapter
-        lv_listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            Toast.makeText(applicationContext, parent?.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show()
+            adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                resources.getStringArray(R.array.countries_array)
+            )
+            lv_ListView.adapter = adapter
+            lv_ListView.onItemClickListener =
+                AdapterView.OnItemClickListener { parent, view, position, id ->
+                    Toast.makeText(
+                        applicationContext,
+                        parent?.getItemAtPosition(position).toString(),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            lv_ListView.emptyView = tv_emptyTextView
         }
-        lv_listView.emptyView = tv_emptyTextView
 
-
-        override fun onCreateOptionsMenu(menu: Menu?): Boolean{
+        //this initializes the search feature
+        override fun onCreateOptionsMenu(menu: Menu?): Boolean {
             menuInflater.inflate(R.menu.nav_menu, menu)
 
             val search = menu?.findItem(R.id.nav_search)
-            val searchView = search?.actionVeiw as SearchView
-            searchView.queryHint ="search something"
-            searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            val searchView = search?.actionView as SearchView
+            searchView.queryHint = "search something"
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(query: String?): Boolean {
                     return false
                 }
@@ -41,8 +52,4 @@ class MainActivity : AppCompatActivity() {
             })
             return super.onCreateOptionsMenu(menu)
         }
-
-
-
     }
-}
